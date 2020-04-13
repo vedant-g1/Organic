33e288d999a6e6a023d2abac9b1a6fedbe64b7b3
sai123gopal <sai.gopalsai143@gmail.com>
2020-04-13 17:44:16 +0530

    commit

--- app/src/main/java/com/saigopal/organic/Far_manage_existing.java
+++ app/src/main/java/com/saigopal/organic/Far_manage_existing.java
@@ -4,8 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-import androidx.annotation.ColorRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -16,7 +16,6 @@ public class Far_manage_existing extends Fragment
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragement_manage_existiing, container,false);
-
 
 
 
